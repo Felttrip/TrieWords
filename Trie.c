c5005d8e005ead362095a69c773d1cbec04e5a06
Nathaniel Thompson <nthompson55@gmail.com>
2013-09-13 12:07:05 -0500

    First commit, adding Trie.c

---
+++ Trie.c
@@ -0,0 +1,18 @@
+//Trie data structure testing
+//All code written my Nathaniel Thompson
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//trie structure
+typedef struct _trie
+{
+    char letter; //the letter that the node holds
+    _trie *next[26];//pointers to the rest of the letters
+}trie;
+
+//main function
+int main(void)
+{
+
+}
